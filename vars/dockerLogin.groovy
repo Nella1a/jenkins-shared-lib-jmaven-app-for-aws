ab0b0751bde3377b488c552bc36dfa7665737074
Nella1a <m.allen@gmx.at>
2025-07-02 09:25:42 +0200

    fix strings

--- vars/dockerLogin.groovy
+++ vars/dockerLogin.groovy
@@ -3,6 +3,8 @@
 def call() {
     echo "Dockerhub login ..."
     withCredentials([usernamePassword(credentialsId:'cred-docker', passwordVariable: 'PASS', usernameVariable: 'USER')]){
-        sh "echo ${PASS} | docker login -u ${USER} --password-stdin"
+        sh  ''' 
+             echo "$PASS" | docker login -u "$USER" --password-stdin
+            '''
     }
 }
